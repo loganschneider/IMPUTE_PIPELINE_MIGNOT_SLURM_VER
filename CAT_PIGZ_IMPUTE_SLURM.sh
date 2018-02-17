1e3e58028b3502256b2cd5bf51ec9c5df07404bf
Aditya Ambati <ambati@stanford.edu>
2018-02-16 17:13:58 -0800

    Update CAT_PIGZ_IMPUTE_SLURM.sh

--- CAT_PIGZ_IMPUTE_SLURM.sh
+++ CAT_PIGZ_IMPUTE_SLURM.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+touch CAT_IMPUTE_PIGZ.sh
+chmod 755 CAT_IMPUTE_PIGZ.sh
+cat > 755 CAT_IMPUTE_PIGZ.sh <<- EOF
 #!/bin/bash -l
 #SBATCH --job-name=catimpute_files
 #SBATCH --mem-per-cpu=4000
@@ -6,7 +10,8 @@
 #SBATCH --time=12:00:00
 mkdir impute_info
 module load pigz/2.3.4
-mv CHR$SLURM_ARRAY_TASK_ID\_"$1".*_* impute_info/
-cat CHR$SLURM_ARRAY_TASK_ID\_"$1".* > CHR$SLURM_ARRAY_TASK_ID\_"$1".impute2
-pigz CHR$$SLURM_ARRAY_TASK_ID\_"$1".impute2
-sbatch --export=ALL
+mv CHR$SLURM_ARRAY_TASK_ID_"$1".*_* impute_info/
+cat CHR$SLURM_ARRAY_TASK_ID_"$1".* > CHR$SLURM_ARRAY_TASK_ID_"$1".impute2
+pigz CHR$$SLURM_ARRAY_TASK_ID_"$1".impute2
+EOF
+
