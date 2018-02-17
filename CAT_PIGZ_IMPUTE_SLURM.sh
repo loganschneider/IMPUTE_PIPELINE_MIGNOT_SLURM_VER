e46ba4dffedb52b077bcd9c9319da357ed4ce9e5
Aditya Ambati <ambati@stanford.edu>
2018-02-16 17:43:00 -0800

    Update CAT_PIGZ_IMPUTE_SLURM.sh

--- CAT_PIGZ_IMPUTE_SLURM.sh
+++ CAT_PIGZ_IMPUTE_SLURM.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 touch CAT_IMPUTE_PIGZ.sh
 chmod 755 CAT_IMPUTE_PIGZ.sh
-cat > 755 CAT_IMPUTE_PIGZ.sh <<- EOF
+#MV_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".*_* impute_info/`
+#CAT_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".* > CHR$SLURM_ARRAY_TASK_ID\_"$1".impute2`
+#PIGZ_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".impute2`
+cat > CAT_IMPUTE_PIGZ.sh <<- EOF
 #!/bin/bash -l
 #SBATCH --job-name=catimpute_files
 #SBATCH --mem-per-cpu=4000
 #SBATCH --array=1-22
 #SBATCH --account=mignot
 #SBATCH --time=12:00:00
-mkdir impute_info
 module load pigz/2.3.4
-mv CHR$SLURM_ARRAY_TASK_ID_"$1".*_* impute_info/
-cat CHR$SLURM_ARRAY_TASK_ID_"$1".* > CHR$SLURM_ARRAY_TASK_ID_"$1".impute2
-pigz CHR$$SLURM_ARRAY_TASK_ID_"$1".impute2
+mv CHR\$SLURM_ARRAY_TASK_ID\_$1.*_* impute_info/
+cat CHR\$SLURM_ARRAY_TASK_ID\_$1.* > CHR\$SLURM_ARRAY_TASK\_ID_$1.impute2
+pigz CHR\$SLURM_ARRAY_TASK_ID\_$1.impute2
 EOF
-
+sbatch --export=ALL CAT_IMPUTE_PIGZ.sh
