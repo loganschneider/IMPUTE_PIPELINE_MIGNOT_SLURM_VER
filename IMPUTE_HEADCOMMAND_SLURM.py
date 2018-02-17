f5b5ce1e83ad95385864a70b85cc8fde21581639
Aditya Ambati <ambati@stanford.edu>
2018-02-16 16:04:52 -0800

    Update IMPUTE_HEADCOMMAND_SLURM.py

--- IMPUTE_HEADCOMMAND_SLURM.py
+++ IMPUTE_HEADCOMMAND_SLURM.py
@@ -7,9 +7,9 @@ with open('chr_sizes.txt') as chr_in:
 		chr_ = re.sub('chr', '', line_parse[0])
 		size = line_parse[1]
 		if len(line_parse[1]) < 9:
-			makecommand = "./IMPUTE_LOOP_PHASE1_STEP3.sh "+chr_+' '+str(int(line_parse[1][:2])+1)+' '+sys.argv[1]+' '+sys.argv[2]
+			makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:2])+1)+' '+sys.argv[1]+' '+sys.argv[2]
 			subprocess.Popen(makecommand, shell=True)
 		else:
-			makecommand = "./IMPUTE_LOOP_PHASE1_STEP3.sh "+chr_+' '+str(int(line_parse[1][:3])+1)+' '+sys.argv[1]+' '+sys.argv[2]
+			makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:3])+1)+' '+sys.argv[1]+' '+sys.argv[2]
 			subprocess.Popen(makecommand, shell=True)
 		print makecommand
