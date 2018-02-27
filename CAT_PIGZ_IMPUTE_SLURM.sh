09d674ca16386da845f6ec86a3e4155dc2c65ff6
Logan Schneider <loganschneider@users.noreply.github.com>
2018-02-26 16:38:53 -0800

    Update CAT_PIGZ_IMPUTE_SLURM.sh

--- CAT_PIGZ_IMPUTE_SLURM.sh
+++ CAT_PIGZ_IMPUTE_SLURM.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+mkdir -p ~/impute_info
+
 touch CAT_IMPUTE_PIGZ.sh
 chmod 755 CAT_IMPUTE_PIGZ.sh
-#MV_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".*_* impute_info/`
-#CAT_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".* > CHR$SLURM_ARRAY_TASK_ID\_"$1".impute2`
-#PIGZ_COM=`CHR$SLURM_ARRAY_TASK_ID\_"$1".impute2`
+#MV_COM=`CHR${SLURM_ARRAY_TASK_ID}\_"${1}".*\_* impute_info/`
+#CAT_COM=`CHR${SLURM_ARRAY_TASK_ID}\_"${1}".* > CHR${SLURM_ARRAY_TASK_ID}\_"${1}".impute2`
+#PIGZ_COM=`CHR${SLURM_ARRAY_TASK_ID}\_"${1}".impute2`
 cat > CAT_IMPUTE_PIGZ.sh <<- EOF
 #!/bin/bash -l
 #SBATCH --job-name=catimpute_files
@@ -12,8 +14,8 @@ cat > CAT_IMPUTE_PIGZ.sh <<- EOF
 #SBATCH --account=mignot
 #SBATCH --time=12:00:00
 module load pigz/2.3.4
-mv CHR\$SLURM_ARRAY_TASK_ID\_$1.*_* impute_info/
-cat CHR\$SLURM_ARRAY_TASK_ID\_$1.* > CHR\$SLURM_ARRAY_TASK\_ID_$1.impute2
-pigz CHR\$SLURM_ARRAY_TASK_ID\_$1.impute2
+mv CHR\${SLURM_ARRAY_TASK_ID}_${1}.*_* impute_info/
+cat CHR\${SLURM_ARRAY_TASK_ID}_${1}.* > CHR\${SLURM_ARRAY_TASK_ID}_${1}.impute2
+pigz CHR\${SLURM_ARRAY_TASK_ID}_${1}.impute2
 EOF
 sbatch --export=ALL CAT_IMPUTE_PIGZ.sh
