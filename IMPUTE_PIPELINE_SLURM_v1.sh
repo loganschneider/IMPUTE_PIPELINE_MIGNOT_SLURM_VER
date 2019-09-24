e06ff94145c48f1c07ea0451aad1019b839025c6
Aditya Ambati <ambati@stanford.edu>
2019-09-23 19:38:56 -0700

    logic update

--- IMPUTE_PIPELINE_SLURM_v1.sh
+++ IMPUTE_PIPELINE_SLURM_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "################################################################################################################" 
+echo "################################################################################################################"
 echo "#                            IMPUTE PIPELINE VERSION 1 SCG4  Version date August 2019 SLURM VERSION            #"
 echo "#                                     SCG4 utilizing parallel computation SLURM VERSION 2                      #"
 echo "#                                              Aditya Ambati                                                   #"
@@ -8,30 +8,31 @@ echo "##########################################################################
 
 echo "`date` STARTING IMPUTATION "
 echo " `date` CHECKING IF PLINK FILES SPLIT BY CHR EXIST in CURRENT DIR  "
-if [[ -f $PWD/${1}_CHR2.bed ]];then
+if [[ -f $PWD/$1_CHR2.bed ]];then
    echo " `date` PLINK FILES HAVE ALREADY BEEN SPLIT >>>> PROCEEDING TO SHAPEIT TASK"
-   if  [[ -f $PWD/${1}_CHR2.haps ]];then
-      ./SHAPEIT_ARRAY_TASK_SLURM.sh $1 $job0
-      JOBID1=$(sbatch --export=ALL shapeit_array.sh)
-      if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then 
-         echo "`date` Job(s) submission failed."
-         echo ${JOBID1}
-         exit 1
-      else
-         job1=$(echo ${JOBID1} | grep -oh "[1-9][0-9]*$")
-      fi
-      echo "`date` GENOTYPES HAVE BEEN PHASED >>>> PROCEEDING TO IMPUTATION"
-      python IMPUTE_HEADCOMMAND_SLURM.py $1 $job1
+   if  [[ -f $PWD/$1_CHR2.haps ]];then
+        echo "`date` GENOTYPES HAVE BEEN PHASED >>>> PROCEEDING TO IMPUTATION"
+        python IMPUTE_HEADCOMMAND_SLURM.py $1 
    else
-      echo "`date` PHASED GENOTYPES HAVE BEEN FOUND >>>> PROCEEDING TO IMPUTATION"
-      job1=000
+        echo "`date` PLINK FILES HAVE ALREADY BEEN SPLIT BUT NOT PHASED >>>> PROCEEDING TO PHASING"
+        ./SHAPEIT_ARRAY_TASK_SLURM.sh $1 $job0
+        JOBID1=$(sbatch --export=ALL shapeit_array.sh)
+        if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then
+            echo "`date` Job(s) submission failed."
+            echo ${JOBID1}
+            exit 1
+        else
+            job1=$(echo ${JOBID1} | grep -oh "[1-9][0-9]*$")
+        fi
+      
+      echo "`date` IMPUTATION IS QUEUED >>>> PROCEEDING TO IMPUTATION"
       python IMPUTE_HEADCOMMAND_SLURM.py $1 $job1
    fi
 else
    echo "`date` PLINK FILES ARE BEING SPLIT BY CHR"
    ./PLINK_SPLIT_SLURM.sh $1
    JOBID0=$(sbatch --export=ALL PLINK_SPLIT.sh)
-   if ! echo ${JOBID0} | grep -q "[1-9][0-9]*$"; then 
+   if ! echo ${JOBID0} | grep -q "[1-9][0-9]*$"; then
       echo "Job(s) submission failed."
       echo ${JOBID0}
       exit 1
@@ -41,7 +42,7 @@ else
    echo "`date` PHASING WITH SHAPEIT "
    ./SHAPEIT_ARRAY_TASK_SLURM.sh $1 $job0
    JOBID1=$(sbatch --export=ALL shapeit_array.sh)
-   if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then 
+   if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then
       echo "Job(s) submission failed."
       echo ${JOBID1}
       exit 1
