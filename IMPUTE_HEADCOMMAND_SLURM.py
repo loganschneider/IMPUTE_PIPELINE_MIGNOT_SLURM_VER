9c39835fd2ee3d08380d085f880e6f5830ad4543
Aditya Ambati <ambati@stanford.edu>
2019-09-24 10:40:28 -0700

    Added option to check for job dependency

--- IMPUTE_HEADCOMMAND_SLURM.py
+++ IMPUTE_HEADCOMMAND_SLURM.py
@@ -6,10 +6,15 @@ with open('chr_sizes.txt') as chr_in:
 		line_parse = line.strip().split('\t')
 		chr_ = re.sub('chr', '', line_parse[0])
 		size = line_parse[1]
-		if len(line_parse[1]) < 9:
-			makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:2])+1)+' '+sys.argv[1]+' '+sys.argv[2]
-			subprocess.Popen(makecommand, shell=True)
-		else:
-			makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:3])+1)+' '+sys.argv[1]+' '+sys.argv[2]
-			subprocess.Popen(makecommand, shell=True)
+		try:
+			if len(line_parse[1]) < 9:
+				makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:2])+1)+' '+sys.argv[1]+' '+sys.argv[2]
+			else:
+				makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:3])+1)+' '+sys.argv[1]+' '+sys.argv[2]
+		except:
+			if len(line_parse[1]) < 9:
+				makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:2])+1)+' '+sys.argv[1]
+			else:
+				makecommand = "./IMPUTE_LOOP_SLURM.sh "+chr_+' '+str(int(line_parse[1][:3])+1)+' '+sys.argv[1]
+		subprocess.Popen(makecommand, shell=True)
 		print makecommand
